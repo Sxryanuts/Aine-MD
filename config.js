56182b626d11e626674f42bdec514bcfbe57bb23
Sxryanuts <78062285+Sxryanuts@users.noreply.github.com>
2022-08-21 19:17:25 +0800

    update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'sekay'
-  var sticker_author = 'Iyh'
+  var sticker_name = 'iyahh'
+  var sticker_author = 'sky'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
